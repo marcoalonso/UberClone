52c0ae5fd3bb7ac6f46dadccc5946c2f7a77d76d
Marco Alonso <marcoalonso@MacBook-Air-de-Marco.local>
2024-02-14 10:13:41 -0600

    Getting Started

---
+++ UberClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UberClone
+//
+//  Created by Marco Alonso on 14/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Marco Alonso on 14/02/24.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
